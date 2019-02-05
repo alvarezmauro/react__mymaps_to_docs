189480254f504e5af148c49a6867661d63cf6d90
Mauro Alvarez <contact@mauroalvarez.com>
2019-02-05 12:49:10 +1100

    Added utility file that has two function: - fromKmlToJs: Will take an KML/XML and return and JS obj representation of it - getMapKml: Will take a MyMap URL (has to be public) and return a obj representation of its KML/XML Data

---
+++ src/utils/KmlReader.js
@@ -0,0 +1,52 @@
+import convert from "xml-js";
+
+/**
+ * Get the information of the map specified in the URL
+ * ! The map of the URL passed to this function needs to be public
+ *
+ * @param mapURL URL of the map
+ */
+export async function getMapKml(mapURL) {
+  // console.log(mapURL);
+
+  const mapKmlFile = await fetch(mapURL);
+  const mapKmlText = await mapKmlFile.text();
+  const mapKmlData = await fromKmlToJs(mapKmlText);
+
+  console.log(mapKmlData);
+
+  return mapKmlData;
+}
+
+/**
+ * Transform a KML/XML file to an object
+ *
+ * @param mapKmlText KML/XML of the map
+ */
+export async function fromKmlToJs(mapKmlText) {
+  let mapKmlData = {
+    status: "success",
+    message: "success",
+    data: {}
+  };
+
+  try {
+    let mapKmlObj = convert.xml2json(mapKmlText, { compact: true, spaces: 4 });
+    mapKmlObj = JSON.parse(mapKmlObj);
+    mapKmlObj = mapKmlObj.kml.Document;
+
+    mapKmlData = {
+      status: "success",
+      message: "success",
+      data: mapKmlObj
+    };
+  } catch (error) {
+    mapKmlData = {
+      status: "error",
+      message: error,
+      data: {}
+    };
+  }
+
+  return mapKmlData;
+}
